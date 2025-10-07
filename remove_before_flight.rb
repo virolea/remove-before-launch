465e0e1fde5d8a46e379cb08681276b582314d55
Vincent Rolea <3525369+virolea@users.noreply.github.com>
2025-10-07 10:59:17 +0200

    Remove test text

--- remove_before_flight.rb
+++ remove_before_flight.rb
@@ -1,21 +1,5 @@
 # frozen_string_literal: true
 
-# MARKDOWN = <<~MARKDOWN
-#   This is a test to see if the parser can look for the different unchecked TODOs that can be bundled in a PR description.
-#
-#   - [ ] This is an unchecked TODO
-#   - [x] This is a checked TODO
-#   - [ ] Another unchecked TODO
-#
-#   Some more text here.
-#
-#   - [ ] Yet another unchecked TODO
-#
-#   End of the markdown content.
-# MARKDOWN
-
-
-
 REGEX = /^\s*-\s+\[\s\]\s+(.+)$/.freeze
 
 pull_request_body = ARGV[0]
